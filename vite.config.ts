9b782e16638961e4e031c63af4a899a8a5309c4e
MirjamElad <mirjamelad@gmail.com>
2025-01-26 14:47:30 +0100

    adding unit test WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
@@ -9,5 +10,13 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/__tests__/setup.ts',
     exclude: ["node_modules", 'src/TestApp/**/*'],
+    deps: {
+      inline: ['adax-core'], // Force Vitest to inline adax-core
+    }
+  },
+  resolve: {
+    alias: {
+      'adax-core': path.resolve(__dirname, './node_modules/adax-core/dist'), // Adjust the path if needed
+    },
   },
 });
